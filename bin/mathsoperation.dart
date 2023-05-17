4a21e5b15d88d7fdc71d56424cbb92cf9ea3cc37
nadheerpt <ptnadheer@gamil.com>
2023-05-17 13:19:52 +0530

    commit

---
+++ bin/mathsoperation.dart
@@ -0,0 +1,30 @@
+void add(){
+  int a = 10, b = 20 ;
+  print("sum = ${a+b}");
+
+}
+
+void sub(){
+  int a = 10, b = 20 ;
+  print("sum = ${a-b}");
+
+}
+
+void multi(){
+  int a = 10, b = 20 ;
+  print("sum = ${a*b}");
+
+}
+
+void div(){
+  int a = 10, b = 20 ;
+  print("sum = ${a/b}");
+
+}
+
+void main(){
+  add();
+  sub();
+  multi();
+  div();
+}
---
+++ bin/operators.dart
@@ -0,0 +1,27 @@
+void main(){
+  dynamic a =20, b = 10;
+  print("a+b=${a+b}");
+  print("a-b=${a-b}");
+  print("a*b=${a*b}");
+  print("a/b=${a/b}");
+  print("a%b=${a%b}");
+  print("a~/b=${a~/b}");
+  print("a=b  a= ${a=b}");
+  print("__________");
+  print("a+=b  a+= ${a+=b}");
+  print("a-=b  a-= ${a-=b}");
+  print("a*=b  a*= ${a*=b}");
+  print("a/=b  a/= ${a/=b}");
+  print("a%=b  a%= ${a%=b}");
+
+  //relational operator
+  int c=100,d=70;
+  print("c<d  -> ${c<d}");
+  print("c>d  -> ${c>d}");
+  print("c<=d -> ${c<=d}");
+  print("c>=d -> ${c>=d}");
+  print("c==d  -> ${c==d}");
+  print("c!=d  -> ${c!=d}");
+
+
+}
